49fe88022cbec59d2b60814e2c3d30de366e6dad
hoyhe <hoyhe@thoughtworks.com>
2019-11-23 23:14:06 +0800

    feature: add search to projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,24 @@
   $(function() {
     $('.button-collapse').sidenav();
     $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('#search').on('input', () => {
+      const search = $('#search')
+        .val()
+        .trim()
+        .toLowerCase();
+      $('[repo-name]').each(function() {
+        const repoName = $(this)
+          .attr('repo-name')
+          .trim();
+        if (search === '') {
+          $(this).show();
+        } else if (!RegExp(search).test(repoName.toLowerCase())) {
+          $(this).hide();
+        } else {
+          $(this).show();
+        }
+      });
+    });
   });
 })(jQuery);
