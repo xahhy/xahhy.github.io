7a407b8c50de90bc2eafbe741c9b48f89629dcb3
hoyhe <hoyhe@thoughtworks.com>
2019-11-23 23:27:50 +0800

    feature: add debounce to search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,34 @@
 (function($) {
   $(function() {
+    const debounce = (fn, ms = 0) => {
+      let timeoutId;
+      return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn.apply(this, args), ms);
+      };
+    };
+
     $('.button-collapse').sidenav();
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
-    $('#search').on('input', () => {
-      const search = $('#search')
-        .val()
-        .trim()
-        .toLowerCase();
-      $('[repo-name]').each(function() {
-        const repoName = $(this)
-          .attr('repo-name')
-          .trim();
-        if (search === '') {
-          $(this).show();
-        } else if (!RegExp(search).test(repoName.toLowerCase())) {
-          $(this).hide();
-        } else {
-          $(this).show();
-        }
-      });
-    });
+    $('#search').on(
+      'input',
+      debounce(() => {
+        const search = $('#search')
+          .val()
+          .trim()
+          .toLowerCase();
+        $('[repo-name]').each(function() {
+          const repoName = $(this)
+            .attr('repo-name')
+            .trim();
+          if (RegExp(search).test(repoName.toLowerCase()) || search === '') {
+            $(this).show();
+          } else {
+            $(this).hide();
+          }
+        });
+      }, 200),
+    );
   });
 })(jQuery);
